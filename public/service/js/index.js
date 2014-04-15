b3106830ba1926bf7cc93a7c53d3f07d8443a841
chumao <chumao@qq.com>
2014-04-15 21:20:18 +0800

    ceshi and service

---
+++ public/service/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".sub1").click(function (event) {
+        $("#notice1").hide();
+    });
+    // $(".content_nav_1").click(function (event) {
+    //     $(".content_nav_1").hide();
+    //     $(".content_nav_1").content_nav_3();
+    //     $(this).addClass('content_nav_1');
+    //     event.preventDefault()
+    // });
+    // $(".content_nav_2").click(function (event) {
+    //     $("#yincang1").hide();
+    //     $("#yincang2").fadeIn();
+    //     $(this).addClass('current_img');
+    //     event.preventDefault()
+    // });
+});
